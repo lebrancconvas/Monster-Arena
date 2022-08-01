4a53d60245d4ecd9bf263ba8a01eef66bbe997af
lebrancconvas <lebrancconvas@gmail.com>
2022-08-01 14:31:50 +0700

    ✨ feat: add submit button

--- web/src/pages/MonsterSelect.tsx
+++ web/src/pages/MonsterSelect.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { useRecoilState } from 'recoil';
 import { monsterNameState } from '../lib/atoms';
 import MonsterTitle from '../components/MonsterTitle';
-import { MonsterButton, WhiteFont } from '../style/Button.style';
+import { MonsterButton, WhiteFont, SubmitButton } from '../style/Button.style';
 import { monsterList } from '../data/monsterList';
+import { Link } from 'react-router-dom';
 
 const MonsterSelect: React.FC = () => {
   const [monsterName, setMonsterName] = useRecoilState(monsterNameState);
@@ -24,6 +25,11 @@ const MonsterSelect: React.FC = () => {
           </MonsterButton>
         )
       })}
+			<Link to="/arena"> 
+				<SubmitButton>
+					Select 
+				</SubmitButton>
+			</Link>
     </div>
   )
 };
--- web/src/style/Button.style.ts
+++ web/src/style/Button.style.ts
@@ -15,6 +15,21 @@ export const MonsterButton = styled.div`
 	}
 `;
 
+export const SubmitButton = styled.div`
+	width: 10%;
+	height: 30px;
+	background-color: orange;
+	border-radius: 10px;
+	box-shadow: 1px 1px 1px 1px black;
+	margin: 10px;
+	cursor: pointer;
+	padding: 5px;
+
+	&:active {
+		transform: scale(0.98);
+	}
+`;
+
 export const WhiteFont = styled.div`
 	color: white;
 	font-size: 16px;
