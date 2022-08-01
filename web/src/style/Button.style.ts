4d3a82b159763b91ef9f50dbabe705c81e003696
lebrancconvas <lebrancconvas@gmail.com>
2022-08-01 14:11:02 +0700

    💄 styled: create style file

---
+++ web/src/style/Button.style.ts
@@ -0,0 +1,21 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const MonsterButton = styled.div`
+	width: 10%;
+	height: 30px;
+	background-color: green;
+	border-radius: 10px;
+	box-shadow: 1px 1px 1px 1px black;
+	margin: 10px;
+	cursor: pointer;
+	padding: 5px;
+
+	&:active {
+		transform: scale(0.98);
+	}
+`;
+
+export const WhiteFont = styled.div`
+	color: white;
+	font-size: 16px;
+`;
